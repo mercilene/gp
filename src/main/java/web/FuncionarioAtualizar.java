1c63f279acf64746e3ff89b4874068764876efa3
Mercilene <mercilenesferreira@gmail.com>
2016-05-11 22:57:50 -0300

    Editar verificar porque não esta trazendo Data de Nascimento e Salário exigindo que se digite novamente

---
+++ src/main/java/web/FuncionarioAtualizar.java
@@ -0,0 +1,46 @@
+package web;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dominio.Funcionario;
+import servico.FuncionarioServico;
+import servico.ServicoException;
+import servico.ValidacaoException;
+
+
+@WebServlet("/funcionario/atualizar")
+public class FuncionarioAtualizar extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private static String DESTINO ="/funcionario/listar.jsp";
+	private static String FORM = "/funcionario/formEditar.jsp";
+	private static String ERRO = "/public/erro.jsp";
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		FuncionarioServico fs = new FuncionarioServico();
+		Funcionario x = Instanciar.funcionario(request);
+		try {
+			fs.validar(x);
+			fs.atualizar(x);
+			List<Funcionario> itens = fs.buscarTodosOrdenadosPorNome();
+			request.setAttribute("itens", itens);
+			request.getRequestDispatcher(DESTINO).forward(request, response);
+		} catch (ServicoException e) {
+			request.setAttribute("msg", e.getMessage());
+			request.getRequestDispatcher(ERRO).forward(request, response);
+		} catch (ValidacaoException e) {
+			request.setAttribute("erros", e.getErros());
+			request.setAttribute("item", x);
+			request.getRequestDispatcher(FORM).forward(request, response);
+		}
+	
+	} 
+
+}
